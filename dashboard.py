cba1bb755ab74ca5a6990d6e53bc7d90f38022db
monu shamota <vivek.jui@gmail.com>
2026-04-17 16:48:22 +0530

    Bump version to 1.02

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ Noting Bot - Flask Web Dashboard Backend
 All API routes for the web UI.
 """
 
-VERSION = "1.01"
+VERSION = "1.02"
 GITHUB_REPO = "vivekjui/Personal_bot"
 GITHUB_REPO_LABEL = "Personal Bot"
 
